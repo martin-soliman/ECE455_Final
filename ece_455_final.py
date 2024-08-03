cd9725d33424cc7bf239fe1baba2a34d3f951441
martin_soliman <m26solim@uwaterloo.ca>
2024-08-03 17:05:50 -0400

    Implemented hyperperiod algorithm for decimal values

--- ece_455_final.py
+++ ece_455_final.py
@@ -1,5 +1,10 @@
 import argparse
 import csv
+import math
+from fractions import Fraction
+from functools import reduce
+
+
 
 def read_inputs(fin):
     try:
@@ -7,14 +12,49 @@ def read_inputs(fin):
             reader = csv.reader(inputs, delimiter=',')
             rows = []
             for row in reader: #row contains string data for current row
-                rows.append([int(num) for num in row]) #list comprehension which converts each row to int values, and appends it to rows 2D sarray
-        print(rows)
+                rows.append([float(num) for num in row]) #list comprehension which converts each row to int values, and appends it to rows 2D sarray
+        return rows
     except FileNotFoundError: #filepath does not exist
         print("File not found :(")
     except Exception as e: #other unexpected errors
         print(f"Error: {e}")
 
-def ouput_results(isSchedulable, preemptions):
+def gcd(a, b):
+    return math.gcd(a, b) #returning gcd of a and b
+
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b) #using gcd to find lcd of a and b
+
+def find_hyperperiod(periods):
+    #since periods can be decimal up to 0.001 precision, we need to use fractions to find LCM 
+
+    fracs = [Fraction(period).limit_denominator() for period in periods] #list comprehension to convert each float into a fraction
+
+    nums = [frac.numerator for frac in fracs] #list comprehension for numerators
+    lcm_num = reduce(lcm, nums) #iteratively finding lcm of ALL numerators
+
+    denoms = [frac.denominator for frac in fracs] #list comprehension for denominators 
+    gcd_denom = reduce(gcd, denoms) #iteratively finding gcd of ALL denominators
+
+    hyperperiod = Fraction(lcm_num, gcd_denom) #the hyperperiod (lcm of all periods) is the fraction lcd_num/gcd_denom
+    return float(hyperperiod)
+
+def simulate_RM(tasks):
+    if(tasks):
+        executions = []
+        periods = []
+        deadlines = []
+        for task in tasks:
+            executions.append(task[0])
+            periods.append(task[1])
+            deadlines.append(task[2])
+
+        hyperperiod = find_hyperperiod(periods) #using reduce function to get LCM of periods
+        print(hyperperiod)
+    else:
+        return [False, None]
+
+def output_results(isSchedulable, preemptions):
     if isSchedulable:
         print(1)
         print(preemptions)
@@ -26,7 +66,10 @@ def main():
     parser.add_argument('file', type=str, help="Path to the txt file to be processed") #Adding argument to parser
     
     args = parser.parse_args() #Assigning args from cmd to args var
-    read_inputs(args.file) #Passing arg associated with inputs txt filepath for processing
+    tasks = read_inputs(args.file) #Passing arg associated with inputs txt filepath for processing
+
+    results = simulate_RM(tasks)
+    #output_results(results[0], results[1])
 
 if __name__ == "__main__":
     main()
