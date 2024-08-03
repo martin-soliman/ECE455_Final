12065d73e3441fd87d1e84c1d9a02d907802a2b0
martin_soliman <m26solim@uwaterloo.ca>
2024-08-03 15:52:34 -0400

    Converting inputs from strings to ints

--- ece_455_final.py
+++ ece_455_final.py
@@ -1,15 +1,25 @@
 import argparse
+import csv
 
 def read_inputs(fin):
     try:
-        with open(fin, 'r') as inputs:
-            content = inputs.read()
-            print("File content:")
-            print(content)
-    except FileNotFoundError: 
+        with open(fin, 'r') as inputs: #file opened
+            reader = csv.reader(inputs, delimiter=',')
+            rows = []
+            for row in reader: #row contains string data for current row
+                rows.append([int(num) for num in row]) #list comprehension which converts each row to int values, and appends it to rows 2D sarray
+        print(rows)
+    except FileNotFoundError: #filepath does not exist
         print("File not found :(")
-    except Exception as e:
+    except Exception as e: #other unexpected errors
         print(f"Error: {e}")
+
+def ouput_results(isSchedulable, preemptions):
+    if isSchedulable:
+        print(1)
+        print(preemptions)
+    else:
+        print(0)
 
 def main():
     parser = argparse.ArgumentParser(description="Parse the inputs txt file") #Instantiating parser object
