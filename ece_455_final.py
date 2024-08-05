fdecd102aa26d17358b8fc159ead1255b6a5340a
martin_soliman <m26solim@uwaterloo.ca>
2024-08-04 22:33:47 -0400

    PC rebooted, lost a lot of commits, this is latest commit I pushed to online repo :(

--- ece_455_final.py
+++ ece_455_final.py
@@ -109,18 +109,22 @@ def simulate_RM(RM_params):
 
     time_chart = [] #array to store task executed at each time step of 0.001s
     preemptions = {} #dict to store preemptions, key is task #, value is num preemptions
-    release_queue = [] #initializing queue of tasks at t = 0 based on task priorities, queue contains task # and remaining exec time
+    release_queue = [] #initializing queue of tasks at t = 0 based on task priorities, queue contains task #, remaining exec time, and deadline
     for key in priorities:
         preemptions[key] = 0
-        release_queue.append([key, executions[key]/TIME_STEP])
+        release_queue.append([key, executions[key]/TIME_STEP, deadlines[key]/TIME_STEP]) 
     
     released = release_queue.pop(0)
     curr_task = released[0]
     t_exec = released[1]
+    curr_deadline = released[2]
     start_time = 0
 
     for i in range(0, int((hyperperiod/TIME_STEP)+1)): #simulating up until hyperperiod
+        if i >= curr_deadline: #ensuring that deadline of curr_task hasn't been missed (>= condition relates to the way I implemented indexing)
+            return
         time_chart.append(curr_task)
+       
         if (i + 1)/INDEX_TO_TIME not in release_times.keys() and (curr_task == -1 or i < (start_time + t_exec - 1)): #no task(s) released AND (no task currently OR curr task is not finished execution)
             continue
 
@@ -128,19 +132,22 @@ def simulate_RM(RM_params):
             for task in release_times[(i + 1)/INDEX_TO_TIME]:
                 if(curr_task == -1 or priorities[task] < priorities[curr_task] or i >= (start_time + t_exec - 1)): #processor is idle, new task is higher priority than curr task, or curr task JUST finished
                     if curr_task != -1 and i < (start_time + t_exec - 1): #if processor isn't idle and curr_task isn't already finished
-                        release_queue.insert(0, [curr_task, executions[curr_task]*INDEX_TO_TIME - (i + 1 - start_time)]) #placing curr task and remaining exec time back on queue
+                        release_queue.insert(0, [curr_task, executions[curr_task]/TIME_STEP - (i + 1 - start_time), curr_deadline]) #placing curr task, remaining exec time back, and deadlines back on queue
                         preemptions[curr_task] += 1 #incrementing preemption count of curr task
+
                     curr_task = task #updating curr task
-                    t_exec = executions[task]*INDEX_TO_TIME #updating remaining exec time
+                    t_exec = executions[task]/TIME_STEP #updating remaining exec time
                     start_time = i + 1 #updating start_time
+                    curr_deadline = start_time + deadlines[task]/TIME_STEP
+
                 else: #place task in queue according to priority
                     count = 0
                     for n in release_queue:
-                        if priorities[task] >= priorities[n]:
+                        if priorities[task] >= priorities[n[0]]:
                             count += 1
                         else:
                             break
-                    release_queue.insert(count, [task, executions[task]*INDEX_TO_TIME])
+                    release_queue.insert(count, [task, executions[task]/TIME_STEP, i + 1 + deadlines[task]/TIME_STEP])
                         
         elif i >= (start_time + t_exec - 1): #curr task finished execution
             if release_queue:
@@ -148,6 +155,7 @@ def simulate_RM(RM_params):
                 curr_task = released[0]
                 t_exec = released[1]
                 start_time = i + 1 #storing start time of release
+                curr_deadline = start_time + released[2]
             else:
                 curr_task = -1; #no tasks to release (idle processor)
         
@@ -157,7 +165,7 @@ def simulate_RM(RM_params):
             output.append(preemptions[i])
         else:
             output.append(0)
-    #print(f"Output: {output}")
+
     return output
 
 def output_results(results):
@@ -168,6 +176,9 @@ def output_results(results):
                 print(results[i])
             else:
                 print(f"{results[i]}", end=',')
+    else:
+        print(0)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Parse the inputs txt file") #Instantiating parser object
@@ -176,13 +187,30 @@ def main():
     args = parser.parse_args() #Assigning args from cmd to args var
     tasks = read_inputs(args.file) #Passing arg associated with inputs txt filepath for processing
 
-    #tasks = [[1,3,3],[2,4,5]]
+    # tasks = [[4,50,100], 
+    #          [1,25,100], 
+    #          [4,150,200],
+    #          [6,100,100],
+    #          [15,250,300],
+    #          [15,250,300],
+    #          [2,250,100],
+    #          [6,150,100],
+    #          [10,100,100],
+    #          [2,40,100],
+    #          [3,50,100],
+    #          [1,20,100],
+    #          [2,50,100],
+    #          [1,40,100],
+    #          [4,200,200],
+    #          ]
 
-    RM_params = prep_RM(tasks)
-    if RM_params:
-        results = simulate_RM(RM_params)
+    RM_params = prep_RM(tasks) #preparing data for simulation
+    
+    if RM_params: #if successful, run simulation
+        results = simulate_RM(RM_params) 
     else:
-        results = None
+        results = None    
+    
     output_results(results)
 
 if __name__ == "__main__":
